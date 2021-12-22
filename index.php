eb8ea93fe8800290857e72e5b1b8059322287f04
Po Yu Chen <pyshif@gmail.com>
2021-12-22 19:40:53 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <!-- coolicons -->
+    <link rel="stylesheet" href="assets/fonts/coolicons-v2.5/coolicons.css">
+    <!-- fontawesome -->
+    <script defer src="node_modules/@fortawesome/fontawesome-free/js/all.js"></script>
+    <!-- <link rel="stylesheet" href="node_modules/@fortawesome/fontawesome-free/css/all.css"> -->
+    <!-- material icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+
+<body>
+    <h1>coolicons</h1>
+    <i class="ci-chevron_duo_up"></i>
+    <i class="ci-unfold_less"></i>
+    <h1>fontawesome</h1>
+    <i class="fas fa-archway"></i>
+    <h1>material icons</h1>
+    <span class="material-icons">done</span>
+    <span class="material-icons">thumb_up</span>
+</body>
+
+</html>
